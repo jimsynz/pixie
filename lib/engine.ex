54071f764f207c0a345484e3cbe124bc3e7f72c0
James Harton <james@resistor.io>
2015-08-25 16:46:52 +1200

    Get rid of Faye-style engine code.

--- lib/engine.ex
+++
@@ -1,9 +0,0 @@
-defprotocol Pixie.Engine do
-  def create_client engine
-  def destroy_client engine, client_id
-  def ping engine, client_id
-  def client_exists? engine, client_id
-  def subscribe engine, client_id, channel
-  def unsubscribe engine, client_id, channel
-  def publish engine, message, channels
-end
