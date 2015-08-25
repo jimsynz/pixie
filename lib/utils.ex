2d5c58fdccd055daf01439118ae42890522b1125
James Harton <james@resistor.io>
2015-08-25 21:35:59 +1200

    Optional capitalized first letter in camelize.

--- lib/utils.ex
+++ lib/utils.ex
@@ -2,12 +2,19 @@ require Mix.Utils
 
 defmodule Pixie.Utils do
 
-  def camelize(term) when is_atom(term) do
-    term |> Atom.to_string |> camelize |> String.to_atom
+  def camelize(term), do: camelize(term, false)
+
+  def camelize(term, upper_first) when is_atom(term) do
+    term |> Atom.to_string |> camelize(upper_first) |> String.to_atom
   end
 
-  def camelize(term) when is_bitstring(term) do
+  def camelize(term, upper_first) when is_bitstring(term) and upper_first == true do
     term |> Mix.Utils.camelize
+  end
+
+  def camelize(term, upper_first) when is_bitstring(term) and upper_first == false do
+    {first, rest} = term |> camelize(true) |> String.split_at(1)
+    "#{String.downcase first}#{rest}"
   end
 
   def underscore(term) when is_atom(term) do
@@ -18,12 +25,14 @@ defmodule Pixie.Utils do
     term |> Mix.Utils.underscore
   end
 
-  def camelize_keys map do
+  def camelize_keys(m), do: camelize_keys(m, false)
+
+  def camelize_keys map, upper_first do
     Enum.reduce map, %{}, fn
       ({key, value}, acc) when is_atom(key) ->
-        Map.put acc, camelize(key), value
+        Map.put acc, camelize(key, upper_first), value
       ({key, value}, acc) when is_bitstring(key)->
-        Map.put acc, camelize(key), value
+        Map.put acc, camelize(key, upper_first), value
       ({key, value}, acc) ->
         Map.put acc, key, value
     end
