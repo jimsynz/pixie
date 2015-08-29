e86b2f87185e4fcd568cd321997e254530807ae2
James Harton <james@resistor.io>
2015-08-29 22:47:51 +1200

    Bump to `0.0.2` now that we appear to support the complete protocol over long polling.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Pixie.Mixfile do
   def project do
     [
       app: :pixie,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
