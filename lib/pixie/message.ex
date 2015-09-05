45c5bb59587ab8f27797b121a226f48f69a03c7f
James Harton <james@resistor.io>
2015-09-06 10:19:41 +1200

    Add documentation for message structs.

--- lib/pixie/message.ex
+++ lib/pixie/message.ex
@@ -1,10 +1,24 @@
 defmodule Pixie.Message do
   alias Pixie.Utils.Map, as: MapUtils
 
+  @moduledoc """
+  This module handles dispatch of incoming messages to their corresponding
+  message structs.
+  """
+
+  @doc """
+  Take `message` (the map output from JSON decoding) and apply the following
+  transformations:
+
+    - Convert all keys to lower case.
+    - Convert all keys to atoms.
+
+  Then dispatch to the correct message module, based on message type.
+  """
   def init message do
     message
-      |> MapUtils.atomize_keys
       |> MapUtils.underscore_keys
+      |> MapUtils.atomize_keys
       |> do_init
   end
 
