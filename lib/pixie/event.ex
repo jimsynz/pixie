3461bc8c02be2d63cf01856e81033f72d19343a6
James Harton <james@resistor.io>
2015-09-06 10:52:30 +1200

    Add documentation for `Pixie.Event`

--- lib/pixie/event.ex
+++ lib/pixie/event.ex
@@ -1,3 +1,16 @@
 defmodule Pixie.Event do
-  defstruct client: nil, message: nil, response: nil
+  defstruct client: nil, message: nil, response: nil, meta: %{}
+
+  @moduledoc """
+  This module defines the event struct, which is created for all incoming
+  messages from clients and passed into the `incoming` function of extensions.
+
+  This struct contains the following fields:
+    - `:client` the PID of the `Pixie.Client` process.
+    - `:message` a `Pixie.Message.*` struct, containing the incoming message.
+    - `:response` a `Pixie.Response.*` struct, containing the response to be
+      sent to the client.
+    - `:meta` a map within which you can store arbitrary information during
+      the course of extension processing.
+  """
 end
