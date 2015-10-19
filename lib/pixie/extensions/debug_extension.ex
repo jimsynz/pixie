33168d61f126e26079a076dbe4247f58499c8ff1
James Harton <james@resistor.io>
2015-10-19 21:38:18 +1300

    Add moduledoc for debug extension.

--- lib/pixie/extensions/debug_extension.ex
+++ lib/pixie/extensions/debug_extension.ex
@@ -2,6 +2,12 @@ defmodule Pixie.DebugExtension do
   use Pixie.Extension
   require Logger
 
+  @moduledoc """
+  Use this extension for debugging only - it logs every event to `Logger`
+  which has the unfortunate side effect of exploding your VM when there's
+  logs of traffic.
+  """
+
   def incoming event do
     Logger.debug "#{inspect __MODULE__}.incoming: #{inspect event}"
     event
